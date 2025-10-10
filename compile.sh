8dab9f0a6e2ba9f0eed5321654eaa17d32f28527
heyenleon <leon.heyen@haw-hamburg.de>
2025-10-10 06:33:44 +0200

    wrote build script

---
+++ compile.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+out="o/replace"
+compilation_log="compilation.log"
+
+outpath=$(echo $out)
+outdir=$(dirname $outpath)
+
+mkdir -p "$outdir"
+gcc replace.c -v --output="$out" > "$compilation_log" 2>&1; echo ""
+if [ $? = "0" ]; then
+    echo "Compiled successfully!"
+    echo "Won't show compilation log"
+    echo -e "(but it's saved under \"$compilation_log\")\n"
+    chmod +x "$outpath"
+    if [ "$1" = "--and-run-using" ]; then
+        echo "Running compiled binary using:"
+        echo -e "$(echo $outpath | sed s"|$(pwd)|.|")\
+            $(echo "$*" | sed s'|--and-run-using||')\n"
+        "$outpath" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" 
+    fi
+else
+    echo "Compilation failed."
+    echo "Do you want to look at the log? (y/n): "
+    read answer
+    if [ "$answer" = y ]; then cat "$compilation_log"; fi
+fi
+
